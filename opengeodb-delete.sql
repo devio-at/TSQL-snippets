081d42961c0df29833a190b34b175e4bc26f077f
devio-at <github@devio.at>
2018-05-21 12:10:49 +0200

    OpenGeoDB

---
+++ opengeodb-delete.sql
@@ -0,0 +1,8 @@
+truncate table geodb_type_names;
+truncate table geodb_textdata;
+truncate table geodb_intdata;
+truncate table geodb_hierarchies;
+truncate table geodb_floatdata;
+truncate table geodb_coordinates;
+truncate table geodb_changelog;
+delete geodb_locations;
