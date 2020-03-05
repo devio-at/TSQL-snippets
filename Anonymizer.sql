7443cef20b2f8af307a84c4eb1f64e7f067375ea
devio-at <github@devio.at>
2020-03-05 09:05:46 +0100

    uppercase

--- Anonymizer.sql
+++ Anonymizer.sql
@@ -8,14 +8,14 @@
 
 	UPDATE	dbo.PersonTable
 	SET		LastName = ' Zy' 
-				+ substring(@cons, (ABS(CHECKSUM(NewId())) % len(@cons))+1, 1)
-				+ substring(@vow, (ABS(CHECKSUM(NewId())) % len(@vow))+1, 1)
-				+ substring(@cons, (ABS(CHECKSUM(NewId())) % len(@cons))+1, 1)
-				+ substring(@cons, (ABS(CHECKSUM(NewId())) % len(@cons))+1, 1)
-				+ substring(@vow, (ABS(CHECKSUM(NewId())) % len(@vow))+1, 1)
-				+ substring(@cons, (ABS(CHECKSUM(NewId())) % len(@cons))+1, 1),
+				+ SUBSTRING(@cons, (ABS(CHECKSUM(NewId())) % LEN(@cons))+1, 1)
+				+ SUBSTRING(@vow, (ABS(CHECKSUM(NewId())) % LEN(@vow))+1, 1)
+				+ SUBSTRING(@cons, (ABS(CHECKSUM(NewId())) % LEN(@cons))+1, 1)
+				+ SUBSTRING(@cons, (ABS(CHECKSUM(NewId())) % LEN(@cons))+1, 1)
+				+ SUBSTRING(@vow, (ABS(CHECKSUM(NewId())) % LEN(@vow))+1, 1)
+				+ SUBSTRING(@cons, (ABS(CHECKSUM(NewId())) % LEN(@cons))+1, 1),
 			FirstName = 'Zy'
-				+ substring(@cons1, (ABS(CHECKSUM(NewId())) % len(@cons1))+1, 1)
-				+ substring(@vow1, (ABS(CHECKSUM(NewId())) % len(@vow1))+1, 1)
-				+ substring(@cons1, (ABS(CHECKSUM(NewId())) % len(@cons1))+1, 1),
+				+ SUBSTRING(@cons1, (ABS(CHECKSUM(NewId())) % LEN(@cons1))+1, 1)
+				+ SUBSTRING(@vow1, (ABS(CHECKSUM(NewId())) % LEN(@vow1))+1, 1)
+				+ SUBSTRING(@cons1, (ABS(CHECKSUM(NewId())) % LEN(@cons1))+1, 1),
 	WHERE   [AnonymizeCondition] = 1
